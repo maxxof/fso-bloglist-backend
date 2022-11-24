71a01439884aeab40fa06e3c95291403ef48aabc
maxxof <maxim.egorov.soccer@gmail.com>
2022-11-24 15:23:58 +0200

    added config.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const config = require('./utils/config.js')
 
 const blogSchema = new mongoose.Schema({
   title: String,
@@ -38,7 +39,6 @@ app.post('/api/blogs', (request, response) => {
     })
 })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
 })
