74d43d156ca3ee6770a5ddbdde0fd4fe78b069f4
maxxof <maxim.egorov.soccer@gmail.com>
2022-11-25 15:53:05 +0200

    Ex 4.9 done

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -20,6 +20,11 @@ test('returns correct amount of blogs in JSON', async () => {
   expect(response.body).toHaveLength(blogs.length)
 })
 
+test('unique identifier of blog posts is named "id"', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body[0].id).toBeDefined()
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
