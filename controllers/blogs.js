ee7cef56c6be945b4d3e043b317eba5f2994832f
maxxof <maxim.egorov.soccer@gmail.com>
2022-11-28 15:54:57 +0200

    Ex 4.22 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,51 +2,47 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog.js')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
+const userExtractor = require('../utils/middleware').userExtractor
 
 blogsRouter.get('/', async (req, res) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     res.json(blogs)
 })
   
-blogsRouter.post('/', async (req, res) => {
-    const body = req.body
-    const decodedToken = jwt.verify(req.token, process.env.SECRET)
-    if (!decodedToken.id) {
-      return res.status(401).json({ error: 'token missin or invalid' })
-    }
+blogsRouter.post('/', userExtractor, async (req, res) => {
+  const body = req.body
 
-    if (!body.url || !body.title) {
-      return res.status(400).json({ error: `cannot create a blog withour url and title`})
-    }
+  if (!body.url || !body.title) {
+    return res.status(400).json({ error: `cannot create a blog withour url and title`})
+  }
 
-    const user = await User.findById(decodedToken.id)
+  const user = req.user
 
-    const blog = new Blog({
-      title: body.title,
-      author: body.author,
-      url: body.url,
-      likes: body.likes || 0,
-      user: user._id
-    })
-  
-    const savedBlog = await blog.save()
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
-    res.status(201).json(savedBlog)
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+    user: user._id
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  console.log('didnt save')
+  res.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (req, res) => {
+blogsRouter.delete('/:id', userExtractor, async (req, res) => {
 
-  const decodedToken = jwt.verify(req.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return res.status(401).json({ error: 'token missin or invalid' })
-  }
+  const user = req.user
+  console.log(user)
 
   const blog = await Blog.findById(req.params.id)
   if (!blog) {
     return res.status(400).json({ error: 'blog does not exist'})
   }
-  if (blog.user.toString() === decodedToken.id) {
+  if (blog.user.toString() === user._id.toString()) {
     await Blog.findByIdAndRemove(req.params.id)
     res.status(204).end()
   } else {
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,6 @@
 const logger = require('./logger')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 const requestLogger = (req, res, next) => {
     logger.info('Method:', req.method)
@@ -34,4 +36,21 @@ const tokenExtractor = (req, res, next) => {
   next()
 }
 
-module.exports = { requestLogger, unknownEndpoint, errorHandler, tokenExtractor }
+const userExtractor = async (req, res, next) => {
+
+  const decodedToken = jwt.verify(req.token, process.env.SECRET)
+  if (!req.token || !decodedToken.id) {
+    return res.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+  req.user = user
+  next()
+}
+
+module.exports = 
+{ requestLogger, 
+  unknownEndpoint, 
+  errorHandler, 
+  tokenExtractor, 
+  userExtractor 
+}
